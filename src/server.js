d8a4f0b82e741d50832e23eafc12a27d5da52414
AbhayShaw03 <abhayshaw0301@gmail.com>
2023-11-25 16:02:09 +0530

    Add a default 404 error handler for unknown endpoints

--- src/server.js
+++ src/server.js
@@ -17,7 +17,9 @@ app.use(session({
 app.use('/',TodoRoutes);
 app.use('/',UserRoutes);
 
-
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Endpoint not found" });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
