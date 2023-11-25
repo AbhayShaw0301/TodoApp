dd724a89309a365487936e7776b2474ff0bd37be
AbhayShaw03 <abhayshaw0301@gmail.com>
2023-11-25 15:31:01 +0530

    setting up todo routes and logic for todo CRUD operation in todoController

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express')
+const TodoRoutes = require("../routes/todo.routes");
 const app = express()
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use('/',TodoRoutes);
 
 
 
